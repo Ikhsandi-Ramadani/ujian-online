5c23fd0dd0fb4f714833506d65424796bb415880
Ikhsandi Ramadani <ikhsandiramadani1337@gmail.com>
2023-12-13 03:19:31 +0800

    bug dashboard

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -19,21 +19,31 @@ class DashboardController extends Controller
             ->where('student_id', auth()->guard('student')->user()->id)
             ->get();
 
+        $cekData = ExamGroup::where('student_id', auth()->guard('student')->user()->id)->count();
+
+        if ($cekData > 0) {
+            $last_exam = ExamGroup::with('exam.lesson', 'exam_session', 'exam.teacher', 'student')
+                ->where('student_id', auth()->guard('student')->user()->id)
+                ->latest()->first();
+            $last_grade = Grade::where('exam_id', $last_exam->exam_id)
+                ->where('exam_session_id', $last_exam->exam_session_id)
+                ->where('student_id', auth()->guard('student')->user()->id)
+                ->latest()->first();
+
+            $history = Grade::with('exam', 'exam_session')->where('student_id', auth()->guard('student')->user()->id)
+                ->where('start_time', '!=', null)
+                ->where('end_time', '!=', null)
+                ->orderBy('exam_id', 'DESC')
+                ->latest()->get();
+        } else {
+            $last_exam = null;
+            $last_grade = null;
+            $history = null;
+        }
+
+
         //last exam
-        $last_exam = ExamGroup::with('exam.lesson', 'exam_session', 'exam.teacher', 'student')
-            ->where('student_id', auth()->guard('student')->user()->id)
-            ->latest()->first();
-        $last_grade = Grade::where('exam_id', $last_exam->exam_id)
-            ->where('exam_session_id', $last_exam->exam_session_id)
-            ->where('student_id', auth()->guard('student')->user()->id)
-            ->latest()->first();
 
-
-        $history = Grade::with('exam', 'exam_session')->where('student_id', auth()->guard('student')->user()->id)
-            ->where('start_time', '!=', null)
-            ->where('end_time', '!=', null)
-            ->orderBy('exam_id', 'DESC')
-            ->latest()->get();
 
         //return with inertia
         return inertia('Student/Dashboard/Index', [
